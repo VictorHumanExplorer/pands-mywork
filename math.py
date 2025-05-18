9088137778b69efdaa1d77ac544f96169cde187e
Victor Pajares Liberal <vpajaresl@hotmail.com>
2025-05-18 20:09:27 +0100

    pands-mywork

---
+++ math.py
@@ -0,0 +1,8 @@
+# math
+# messing around with math
+# author Victor Pajares Liberal
+
+floatnum = 0.29
+
+answer = floatnum*100
+print(answer)
---
+++ saymyname.py
@@ -0,0 +1,12 @@
+# saymyname.py
+# messign with inputs
+# Author Victor Pajares Liberal
+
+'''
+name = input("say your name:")
+print("hello " + name)
+'''
+
+number = int( input("enter a number: "))
+answer = number % 4
+print(answer)
